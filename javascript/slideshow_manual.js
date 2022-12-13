2ef63dad025c202afd9431044875ffcde72d7fb4
Erlin Joy Fernandez <71628501+serlindipity@users.noreply.github.com>
2022-12-13 15:27:11 +0400

    Web Layout and Assets Uploaded

---
+++ javascript/slideshow_manual.js
@@ -0,0 +1,41 @@
+let slideIndex = 0;
+showSlides();
+
+// Next-previous control
+function nextSlide() {
+  slideIndex++;
+  showSlides();
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlides();
+}
+
+// Thumbnail image controlls
+function currentSlide(n) {
+  slideIndex = n - 1;
+  showSlides();
+}
+
+function showSlides() {
+  let slides = document.querySelectorAll(".slide");
+
+  if (slideIndex > slides.length - 1) {
+    slideIndex = 0;
+  }
+
+  if (slideIndex < 0){
+    slideIndex = slides.length - 1;
+  } 
+  
+  // hide all slides
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  
+  // show one slide base on index number
+  slides[slideIndex].style.display = "flex";
+
+  
+}
